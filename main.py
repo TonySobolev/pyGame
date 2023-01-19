48c354d4a3c43de448c17356a388be5d924ba29a
TonySobolev <tonykoval95@gmail.com>
2023-01-19 17:59:24 +0300

    adding fonts

--- main.py
+++ main.py
@@ -7,11 +7,40 @@ pygame.display.set_caption("Sobolev Game")
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
 
+square = pygame.Surface((100, 100))
+square.fill('Blue')
+
 running = True
-backgroungColor=  (3, 252, 186)
+backgroungColor=(3, 252, 186)
+
+myfont = pygame.font.Font('fonts/RubikVinyl-Regular.ttf', 60)
+text_surface = myfont.render('SobolevGameDev', True, 'black')
+
+player = pygame.image.load('images/icon.png')
+
+x_coord = 300
+x_dir = 1
+
 while running:
 
     screen.fill(backgroungColor)
+
+    screen.blit(square, (50, 50))
+
+    pygame.draw.circle(screen, 'red', (250,250), 50)
+    pygame.draw.circle(square, 'red', (25, 25), 10)
+    screen.blit(text_surface,(500,0))
+
+
+
+
+    screen.blit(player, (x_coord,100))
+    x_coord+=x_dir
+    if x_coord >600:
+        x_dir = -1
+    elif x_coord <300:
+        x_dir = 1
+
 
     pygame.display.update()
 
@@ -19,8 +48,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                backgroungColor= (92, 211, 250)
+
 
 
