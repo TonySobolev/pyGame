787d1e2b050be3f447d65787a8ed30c5b4e861f2
TonySobolev <tonykoval95@gmail.com>
2023-01-24 12:54:50 +0300

    add enemy, movement of enemy and autospawn, status of collision with enemies and message of lose

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))  #,flags=pygame.NOFRAME  no frame around app
 pygame.display.set_caption("Sobolev Game")
-icon = pygame.image.load('images/icon.png')
+icon = pygame.image.load('images/icon.png').convert_alpha()
 pygame.display.set_icon(icon)
 
 
@@ -17,21 +17,28 @@ backgroungColor=(3, 252, 186)
 myfont = pygame.font.Font('fonts/RubikVinyl-Regular.ttf', 40)
 text_surface = myfont.render('SobolevGameDev', True, 'aquamarine')
 
-bg = pygame.image.load('images/background.png')
+bg = pygame.image.load('images/background.png').convert()
 
 walk_left = [
-    pygame.image.load('images/player_left/left1.png'),
-    pygame.image.load('images/player_left/left2.png'),
-    pygame.image.load('images/player_left/left3.png'),
-    pygame.image.load('images/player_left/left4.png')
+    pygame.image.load('images/player_left/left1.png').convert_alpha(),
+    pygame.image.load('images/player_left/left2.png').convert_alpha(),
+    pygame.image.load('images/player_left/left3.png').convert_alpha(),
+    pygame.image.load('images/player_left/left4.png').convert_alpha()
 ]
 
 walk_right = [
-    pygame.image.load('images/player_right/right1.png'),
-    pygame.image.load('images/player_right/right2.png'),
-    pygame.image.load('images/player_right/right4.png'),
-    pygame.image.load('images/player_right/right3.png')
+    pygame.image.load('images/player_right/right1.png').convert_alpha(),
+    pygame.image.load('images/player_right/right2.png').convert_alpha(),
+    pygame.image.load('images/player_right/right4.png').convert_alpha(),
+    pygame.image.load('images/player_right/right3.png').convert_alpha()
 ]
+
+
+ghost = pygame.image.load('images/ghost.png').convert_alpha()
+
+ghost_list_in_game = []
+
+
 
 player_anim_count = 0  #changing the picture of player for animated motion
 
@@ -41,20 +48,37 @@ player_speed = 10
 player_x = 100
 player_y = 500
 
-is_jump = False
-jump_force = 10 #power of jump
+is_jump = False  #status of jump by def is false, so when jump is pressed animation started
+jump_force = 11 #power of jump
 jump_count = jump_force  #iterration for jump function
 
 bg_sound = pygame.mixer.Sound('sounds/music.mp3')
 bg_sound.play()
 
+ghost_timer = pygame.USEREVENT + 1  #creating timer
+ghost_respawn_time = 2500 # 1k equals 1 s
+pygame.time.set_timer(ghost_timer, ghost_respawn_time)       #settting the timer, 1000 ms equals 1 sec
 
 while running:
 
 
-    screen.blit(bg,(bg_x,0))
-    screen.blit(bg, (bg_x + 1280, 0))
-    keys = pygame.key.get_pressed()
+    screen.blit(bg,(bg_x,0))           #adding first bg image to draw
+    screen.blit(bg, (bg_x + 1280, 0))  #adding second bg image to cycle it
+   # screen.blit(ghost,(ghost_x, 550)) #placing enemy
+
+    player_rect = walk_left[0].get_rect(topleft=(player_x, player_y))   #drawing the rect around player to check collision
+    #ghost_rect = ghost.get_rect(topleft=(ghost_x,550))  #rect around enemy for collison
+    if ghost_list_in_game:   #creating the list of enemies
+        for el in ghost_list_in_game:
+            screen.blit(ghost, el)
+            el.x -= 5
+            if player_rect.colliderect(el):
+                print('You lose')
+
+  #  if player_rect.colliderect(ghost_rect):  #checking if collison happend
+   #     print('You lose')
+
+    keys = pygame.key.get_pressed()       #check what key is pressed
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         screen.blit(walk_left[player_anim_count],(player_x,player_y))
     else:
@@ -96,12 +120,17 @@ while running:
     screen.blit(text_surface,(900,10))
 
 
+
+
     pygame.display.update()
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:      #exit the game
             running = False
             pygame.quit()
+        if event.type == ghost_timer:
+            ghost_list_in_game.append(ghost.get_rect(topleft=(1285,550)))  #creating enemy at starting position
+
 
     clock.tick(70)
 
